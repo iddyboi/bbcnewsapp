fe7e6ae914af1d5c9981940c207a96f7a743529a
idris <idrist2013@gmail.com>
2021-06-03 12:17:29 +0100

    rendered the first article on the page without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { NewsData } from './interfaces/NewsInterface';
 
 import './App.css';
 
 function App() {
+  const [news, setNews] = useState<NewsData | null>(null);
+  useEffect(() => {
+    newsDAta();
+  }, []);
+
+  const newsDAta = async () => {
+    try {
+      const response = await axios.get('data/article-1.json');
+      const data = await response.data;
+      console.log(data);
+      setNews(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className='App'>
       <h1>news app</h1>
+      {news?.title}
+      {news?.body.map(({ type, model }) => (
+        <>
+          <h2>{type}</h2>
+          <p>{model?.text}</p>
+          <img src={model?.url} alt={model?.altText} />
+          <ul>
+            {model?.items?.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </>
+      ))}
     </div>
   );
 }
